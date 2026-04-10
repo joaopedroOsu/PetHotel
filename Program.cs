05438425d82d6f84b024cb11c466efa397b062a8
joao <joaopedroazevedo1980@gmail.com>
2026-04-10 20:49:39 -0300

    PetHotel-teste

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿
+String especie = Console.ReadLine();
+
+String raca = Console.ReadLine();
+
+
+
+Console.WriteLine("=Pet Hotel=");
+Console.WriteLine("especie.....:");
+
+ Console.Write("| Especie:");
+ Console.Write(especie.Trim().ToUpper().PadLeft(15, '.').Substring(0, 15));
+Console.Write("Raca.....:");
+
+Console.Write("| Raca:");
+Console.Write(raca.Trim().ToUpper().PadLeft(15, '.').Substring(0, 15));
+
+
+
+ 
+
