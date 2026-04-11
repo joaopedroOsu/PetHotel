6bf446321d59a03ac4cbce47eee8ca4555615d19
joao <joaopedroazevedo1980@gmail.com>
2026-04-11 13:45:53 -0300

    PethotelCorreto

--- Program.cs
+++ Program.cs
@@ -1,37 +1,52 @@
-﻿Console.WriteLine("=Pet Hotel=");
+﻿Console.ForegroundColor = ConsoleColor.Blue;
+Console.WriteLine("Fixa do Pet");
 Console.Write("especie.....:");
-String especie = Console.ReadLine();
+String especie = Console.ReadLine()!;
 Console.Write("Raca.....:");
-String raca = Console.ReadLine();
+String raca = Console.ReadLine()!;
 Console.Write("Nome do Animal.........:");
-String nome = Console.ReadLine();
+String nome = Console.ReadLine()!;
 Console.Write("Idade Do Animal.....:");
-String idade = Console.ReadLine();
+String idade = Console.ReadLine()!;
 Console.Write("Pelagem do Animal.....:");
-String pelagem = Console.ReadLine();
+String pelagem = Console.ReadLine()!;
 
 // .Insert(15, "|")
 // Console.Write("| Raca:".Insert(, "|"));
+// Console.BackgroundColor = ConsoleColor.White; fica muito ruim
+Console.ForegroundColor = ConsoleColor.Green;
+Console.Write("+=========================================================+");
+Console.ForegroundColor = ConsoleColor.Blue;
+Console.WriteLine("\n|                        Pet Hotel                        |");
+Console.ForegroundColor = ConsoleColor.Green;
+Console.Write("+=========================================================+");
 
- Console.Write("| Especie:");
+Console.ForegroundColor = ConsoleColor.Blue;
+ Console.Write("\n| Especie:");
  Console.Write(especie.Trim().ToUpper().PadLeft(15, '.').Substring(0, 15));
 
+Console.ForegroundColor = ConsoleColor.Blue;
 Console.Write("| Raca:");
-Console.Write(raca.Trim().ToUpper().PadLeft(15, '.').Substring(0, 15));
+Console.Write(raca.Trim().ToUpper().PadLeft(25, '.').Substring(0, 25));
 Console.Write(" |");
+Console.ForegroundColor = ConsoleColor.Green;
+Console.Write("\n+=========================================================+");
 
+Console.ForegroundColor = ConsoleColor.Blue;
 Console.Write("\n| Atende pela alcunha de:.......");
-Console.Write(nome.Trim().ToUpper().PadLeft(15, '.').Substring(0, 15));
+Console.Write(nome.Trim().ToUpper().PadLeft(25, '.').Substring(0, 25));
 Console.Write(" |");
 
+Console.ForegroundColor = ConsoleColor.Blue;
 Console.Write("\n| Idade:");
 Console.Write(idade.Trim().ToUpper());
 Console.Write(" anos(s) |");
 
-
-
+Console.ForegroundColor = ConsoleColor.Blue;
 Console.Write(" Pelagem/cor:");
-Console.Write(pelagem.Trim().ToUpper().PadLeft(15, '.').Substring(0, 15));
+Console.Write(pelagem.Trim().ToUpper().PadLeft(25, '.').Substring(0, 25));
 Console.Write(" |");
 
+Console.ForegroundColor = ConsoleColor.Green;
 Console.Write("\n+=========================================================+");
+Console.ResetColor();
