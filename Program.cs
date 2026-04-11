42ee54b68b0086db9612e33285a692754ec88227
joao <joaopedroazevedo1980@gmail.com>
2026-04-10 22:22:58 -0300

    pethotel

--- Program.cs
+++ Program.cs
@@ -1,21 +1,37 @@
-﻿
+﻿Console.WriteLine("=Pet Hotel=");
+Console.Write("especie.....:");
 String especie = Console.ReadLine();
-
+Console.Write("Raca.....:");
 String raca = Console.ReadLine();
+Console.Write("Nome do Animal.........:");
+String nome = Console.ReadLine();
+Console.Write("Idade Do Animal.....:");
+String idade = Console.ReadLine();
+Console.Write("Pelagem do Animal.....:");
+String pelagem = Console.ReadLine();
 
-
-
-Console.WriteLine("=Pet Hotel=");
-Console.WriteLine("especie.....:");
+// .Insert(15, "|")
+// Console.Write("| Raca:".Insert(, "|"));
 
  Console.Write("| Especie:");
  Console.Write(especie.Trim().ToUpper().PadLeft(15, '.').Substring(0, 15));
-Console.Write("Raca.....:");
 
 Console.Write("| Raca:");
 Console.Write(raca.Trim().ToUpper().PadLeft(15, '.').Substring(0, 15));
+Console.Write(" |");
+
+Console.Write("\n| Atende pela alcunha de:.......");
+Console.Write(nome.Trim().ToUpper().PadLeft(15, '.').Substring(0, 15));
+Console.Write(" |");
+
+Console.Write("\n| Idade:");
+Console.Write(idade.Trim().ToUpper());
+Console.Write(" anos(s) |");
 
 
 
- 
+Console.Write(" Pelagem/cor:");
+Console.Write(pelagem.Trim().ToUpper().PadLeft(15, '.').Substring(0, 15));
+Console.Write(" |");
 
+Console.Write("\n+=========================================================+");
